1f51c19c5db58d57cebf2cf2f671a8f3854fcb04
Aleksandr Kojemyako <alekskojem@gmail.com>
2018-05-25 15:41:48 +0300

    Create migrate article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,23 @@
 class ArticlesController < ApplicationController
-  
+   def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+   render plain: params[:article].inspect
+   end
+
+  def edit
   end
+
+  def destroy
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
